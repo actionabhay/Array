cc69f4f12ab84be772a7d40c9bb996239c1e698d
abhay <abhaychaudhary3999@gmail.com>
2024-02-07 15:08:47 +0530

    sayHello.

---
+++ src/First_program.java
@@ -0,0 +1,9 @@
+
+public class First_program {
+
+	public static void main(String[] args) {
+		System.out.println("First Program");
+
+	}
+
+}
---
+++ src/Sayhello.java
@@ -0,0 +1,9 @@
+
+public class Sayhello {
+
+	public static void main(String[] args) {
+		System.out.println("Say hello to Abhay");
+
+	}
+
+}
